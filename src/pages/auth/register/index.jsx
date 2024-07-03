faa1ee8c6f3324b3aa56bac03380d1411644b4f7
juan-ma526 <jmperez675@gmail.com>
2024-07-03 09:09:59 -0300

    feat-create login,register page-13

---
+++ src/pages/auth/register/index.jsx
@@ -0,0 +1,67 @@
+import { Box, Button, Typography } from "@mui/material";
+import logo from "../images/logoLogin.png";
+import googleIcon from "../images/logoGoogleButtom.png";
+import "../auth.css";
+
+export default function Register() {
+  return (
+    /* Container gral */
+    <Box className="container-gral">
+      {/* Container Card */}
+      <Box className="container-card">
+        {/* First Box */}
+        <Box className="first-box">
+          {/* Text box */}
+          <Box className="text-box">
+            <Typography variant="h5" sx={{ fontWeight: 700 }}>
+              Registrate
+            </Typography>
+            <Typography variant="h6" sx={{ fontWeight: 500 }}>
+              Sumate a ECOSistema
+            </Typography>
+          </Box>
+          {/* Box img */}
+          <Box className="box-img" sx={{ width: "80px", height: "75px" }}>
+            <img src={logo} alt="Logo empresa" />
+          </Box>
+        </Box>
+        {/* Second Box*/}
+        <Box className="second-box">
+          <Typography variant="subtitle1">Registrate con tu cuenta de Gmail</Typography>
+          <Button
+            variant="contained"
+            startIcon={
+              <img
+                src={googleIcon}
+                alt="google icon"
+                style={{
+                  objectFit: "cover",
+                  height: "24px",
+                  width: "24px",
+                  padding: "2px",
+                  backgroundColor: "white",
+                  borderRadius: "100px",
+                }}
+              />
+            }
+            sx={{
+              backgroundColor: "customColors.violeta",
+              borderRadius: "100px",
+              textTransform: "none",
+              width: "200px",
+              height: "40px",
+              ":active": {
+                backgroundColor: "#6E20DB",
+              },
+              ":hover": {
+                backgroundColor: "#6E20DB",
+              },
+            }}
+          >
+            <Typography sx={{ fontSize: "14px", fontWeight: 700 }}>Continuá con Google</Typography>
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
