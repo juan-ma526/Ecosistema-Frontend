dac9a61403e6c39b7dd71e90938a349781c26160
juan-ma526 <jmperez675@gmail.com>
2024-07-05 09:13:04 -0300

    feat-create navbar component-18

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,117 @@
+import LogoImg from "../Navbar/images/logoNavbar.png";
+import "./Navbar.css";
+import { Box, AppBar, Drawer, IconButton, List, Toolbar } from "@mui/material";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+import { ItemList } from "./components/ItemList";
+import { UserOut } from "./components/UserOut";
+import { UserIn } from "./components/UserIn";
+
+const drawerWidth = 258;
+const navItems = [
+  { item: "Inicio", fontWeight: 700, fontSize: 18, fontStyle: "normal" },
+  { item: "Proveedores", fontWeight: 700, fontSize: 18, fontStyle: "normal" },
+  { item: "Publicaciones", fontWeight: 700, fontSize: 18, fontStyle: "normal" },
+  { item: "Iniciá sesión", fontWeight: 700, fontSize: 18, fontStyle: "normal" },
+  {
+    item: "Querés formar parte de la Red de impacto ECO como Proveedor?",
+    fontWeight: 400,
+    fontSize: 18,
+    fontStyle: "italic",
+  },
+  { item: "Registrate", fontWeight: 700, fontSize: 18, fontStyle: "normal" },
+];
+
+function Navbar(props) {
+  // eslint-disable-next-line react/prop-types
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  /* Menu Sidebar */
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{
+        height: "100%",
+        backgroundColor: "customColors.violeta",
+        textAlign: "center",
+        color: "customColors.blanco",
+      }}
+    >
+      {/* Items Sidebar */}
+
+      <List>
+        {navItems.map((object) => (
+          <ItemList
+            item={object.item}
+            fontSize={object.fontSize}
+            fontStyle={object.fontStyle}
+            fontWeight={object.fontWeight}
+            key={object.item}
+          />
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    /* Navbar Container */
+    <Box sx={{ display: "flex", marginBottom: 7 }}>
+      <AppBar sx={{ backgroundColor: "customColors.blanco" }} component="nav">
+        {/* Navbar Components, icon, logo, perfil */}
+
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <IconButton
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ color: "customColors.negro", fontWeight: 600, mr: 2, display: { sm: "none" } }}
+          >
+            {mobileOpen ? <CloseOutlinedIcon /> : <MenuIcon />}
+          </IconButton>
+
+          {/* Box Logo */}
+
+          <Box sx={{ width: "152px", height: "56px" }}>
+            <img src={LogoImg} alt="Logo Empresa" style={{ objectFit: "cover", height: "100%", width: "100%" }} />
+          </Box>
+
+          {/* Profile */}
+
+          {/* //TODO Logic with user Login, if user is logged in, use <UserIn/> else use <UserOut/>*/}
+
+          {/*  <UserOut /> */}
+
+          <UserIn />
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+}
+
+export default Navbar;
