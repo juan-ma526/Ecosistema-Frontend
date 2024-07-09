8c63e5e4f4d9c002ca6e2044387e59887d06e81e
jorgezoto <tito.jorge12@gmail.com>
2024-07-09 19:40:25 -0300

    feat: creo un subcomponente ProvidersHeader

--- src/pages/providers/Providers.jsx
+++ src/pages/providers/Providers.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import {Box, Typography} from '@mui/material';
 import CardProviders from './components/CardProviders/CardProviders';
+import ProvidersHeaders from './components/ProviderHeader/ProvidersHeader';
 import lavanda1 from './images/lavanda1.png';
 import lavanda2 from './images/lavanda2.png';
 import lavanda3 from './images/lavanda3.png';
@@ -76,34 +77,7 @@ function Providers(props) {
     return(
         <>
             <Box className='ProvidersPage'>
-                <Box className='ProvidersHeader' >
-                    <Typography variant='h1' sx={{ 
-                        color: 'customColors.blanco',
-                        fontSize: '18px',
-                        fontWeight: 700,
-                        paddingTop: '120px'
-                        }}>
-                            PROVEEDORES
-                    </Typography>
-                    <Typography sx={{ 
-                        color: 'customColors.blanco',
-                        fontSize: '28px',
-                        fontWeight: 500,
-                        paddingTop: '12px'
-                    }}>
-                        Directorio ECO
-                    </Typography>
-                    <Typography sx={{ 
-                        color: 'customColors.blanco',
-                        fontSize: '24px',
-                        fontWeight: 400,
-                        paddingTop: '12px',
-                        width: '80%'
-                        }}>
-                        Descubrí a quienes comparten tu pasión por 
-                        el impacto positivo y la sostenibilidad
-                    </Typography>
-                </Box>
+                <ProvidersHeaders ></ProvidersHeaders>
                 <Box className='box-title-categories'>
                     <Typography variant='h2' sx={{
                         color: 'customColors.negro',
