6b09a61a4caa6311cfc41bd8642711db66bcd939
juan-ma526 <jmperez675@gmail.com>
2024-07-09 11:27:02 -0300

    feat-add route home page-16

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
+import HomePage from "./pages/landing";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
       </Routes>
