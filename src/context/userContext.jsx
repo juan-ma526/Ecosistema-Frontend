1582eec192d5b7a7c5fc78891303a73f7064d199
juan-ma526 <jmperez675@gmail.com>
2024-07-18 15:45:39 -0300

    feat-create context user-5

---
+++ src/context/userContext.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
