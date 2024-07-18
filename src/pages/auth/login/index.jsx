780644f8cae8062826d7e2dd94dcc9cbc31cfb45
juan-ma526 <jmperez675@gmail.com>
2024-07-18 15:48:45 -0300

    feat-add google buttom oauth-5

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,9 +1,42 @@
 import { Box, Button, Typography } from "@mui/material";
+import { useGoogleLogin } from "@react-oauth/google";
 import logo from "../images/logoLogin.png";
 import googleIcon from "../images/logoGoogleButtom.png";
 import "../Auth.css";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserContext } from "../../../context/userContext";
+import axios from "axios";
 
 export default function Login() {
+  const { setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [token2, setToken2] = useState("");
+
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      const token = tokenResponse.access_token;
+      setToken2(token);
+      const userInfo = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+      });
+      const result = userInfo.data;
+      console.log(result);
+    },
+  });
+  console.log({ token2 });
+  /*  const user = {
+    id: "123asfd34562",
+    name: "Juan Perez",
+    email: "test@gmail.com",
+  };
+ */
+  /*   const handleSubmit = async () => {
+    localStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+    navigate("/");
+  }; */
+
   return (
     /* Container gral */
     <Box className="container-gral">
@@ -13,11 +46,11 @@ export default function Login() {
         <Box className="first-box">
           {/* Text box */}
           <Box className="text-box" sx={{ width: "277px" }}>
-            <Typography sx={{ fontWeight: 700, fontSize: "28px" }}>Inicia Sesión</Typography>
+            ;<Typography sx={{ fontWeight: 700, fontSize: "28px" }}>Inicia Sesión</Typography>
             <Typography sx={{ fontWeight: 600, fontSize: "18px" }}>Seguí disfrutando de ECOSistema</Typography>
           </Box>
           {/* Box img */}
-          <Box className="box-img" sx={{ width: "80px", height: "75px" }}>
+          <Box className="box-img" sx={{ width: "80px", height: "75px", marginTop: "16px" }}>
             <img src={logo} alt="Logo empresa" />
           </Box>
         </Box>
@@ -25,6 +58,7 @@ export default function Login() {
         <Box className="second-box">
           <Typography sx={{ fontWeight: 500 }}>Ingresá con tu cuenta de Gmail</Typography>
           <Button
+            onClick={() => login()}
             variant="contained"
             startIcon={
               <img
