ba4033aea600c7eae0b3bdb2a04c8bd7c723c014
ReloJunior <andresrodriguezab@hotmail.com>
2024-07-29 12:20:21 -0300

    fix: nombre del perfil logueado

--- src/pages/profile/components/ProfileName.jsx
+++ src/pages/profile/components/ProfileName.jsx
@@ -1,25 +1,30 @@
 import { Typography } from "@mui/material";
+import { useContext } from "react";
+import { UserContext } from "../../../context/userContext";
 
 function ProfileName() {
+  const { user } = useContext(UserContext);
+
   return (
     <>
       <section className="container">
-          <Typography
-            variant="h1"
-            sx={{
-              color: "customColors.negro",
-              fontWeight: 700,
-              fontSize: 28,
-              lineHeight: "30px",
-              textAlign: "center",
-              marginTop: "100px"
-            }}
-          >
-            Julieta Pérez
-          </Typography>
+        <Typography
+          variant="h1"
+          sx={{
+            color: "customColors.negro",
+            fontWeight: 700,
+            fontSize: 28,
+            lineHeight: "30px",
+            textAlign: "center",
+            marginTop: "100px"
+          }}
+        >
+          {user ? `${user.nombre} ${user.apellido}` : 'Julieta Perez'}
+        </Typography>
       </section>
     </>
   );
 }
 
 export default ProfileName;
+
