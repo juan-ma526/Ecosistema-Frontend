8ee90b63912b2bc3ad1428aecfa6b5951a5bb721
juan-ma526 <jmperez675@gmail.com>
2024-08-19 11:43:48 -0300

    feat-add route to adminPublications-64

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import EditPublication from "./pages/profile/edit-publication";
 import LoadPublication from "./pages/profile/load-publication";
 import CategoriesPage from "./pages/categories/index";
 import AdminProvidersPage from "./pages/admin-providers/index";
+import AdminPublications from "./pages/admin-publications";
+import CreatePublication from "./pages/admin-publications/createPublication";
+import EditAdminPublication from "./pages/admin-publications/editAdminPublication";
 
 function App() {
   return (
@@ -26,6 +29,9 @@ function App() {
         <Route path="/profile/load" element={<LoadPublication />} />
         <Route path="/categories" element={<CategoriesPage />} />
         <Route path="/adminProviders" element={<AdminProvidersPage />} />
+        <Route path="/adminPublications" element={<AdminPublications />} />
+        <Route path="/admin/createPublication" element={<CreatePublication />} />
+        <Route path="/admin/editPublication" element={<EditAdminPublication />} />
       </Routes>
     </>
   );
